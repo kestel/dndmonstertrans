63683a6c96dc03e82f82b785bdcfe61eff97bae2
Konstantin S <kestel@gmail.com>
2017-04-18 15:02:49 +0300

    Migrate to templates

--- translate_web.py
+++ translate_web.py
@@ -2,7 +2,7 @@
 
 import sys
 # подцепляем словарь из внешнего файла
-import translate_dict as dictionary
+import translate_dict as d
 from translate import replacer
 import bottle
 
@@ -10,40 +10,34 @@ bottle.debug(True)
 
 input_text = None
 
-
 @bottle.route('/dict')
 def print_dict():
-    dict_text = "Вернуться на <a href='/'>главную</a>.<br />\n<h3>Список языков:</h3>\n<br /><pre>"
-    for elem in dictionary.lang_dict:
-        dict_text = dict_text + elem + ": " + dictionary.lang_dict[elem] + "</br />\n"
-    dict_text = dict_text + "</pre>\n<h3>Всё вперемешку:</h3><br /><pre>"
-    for elem in dictionary.all_dict:
-        if elem:
-            dict_text = dict_text + elem + ": " + dictionary.all_dict[elem] + "</br />\n"
-    dict_text = dict_text + "</pre>"
-    return dict_text
+    html = bottle.template("templates/dict.tpl",
+        common_dict=d.common_dict,
+        damage_dict=d.damage_dict,
+        alignment_dict=d.alignment_dict,
+        creature_dict=d.creature_dict,
+        lang_dict=d.lang_dict,
+        condition_dict=d.condition_dict,
+        skill_dict=d.skill_dict,
+        armor_dict=d.armor_dict,
+        weapon_dict=d.weapon_dict )
+    return html
     
     
 @bottle.post('/translate')
 def print_translate():
     input_text = bottle.request.forms.get('text')
     if input_text:
-        return "Вернуться на <a href='/'>главную</a>.<br />\n<pre>"+replacer(input_text, dictionary.all_dict)+"</pre>"
+        return "Вернуться на <a href='/'>главную</a>.<br />\n<pre>"+replacer(input_text, d.all_dict)+"</pre>"
     else:
         return "Вернуться на <a href='/'>главную</a>.<br />\nКажется, вы не ввели текст."
 
 
 @bottle.route('/')
 def print_index():
-    return """
-    <p>Посмотреть <a href="/dict">словарь</a> замены</p>
-    <form action="/translate" method="post">
-    <textarea cols="80" rows="24" name="text"></textarea>
-    <br />
-    <input type="submit" value="Перевести текст"> <input type="reset" value="Очистить поле">
-    </form><br />
-    Исходные коды доступны на <a href="https://github.com/kestel/dndmonstertrans">https://github.com/kestel/dndmonstertrans</a>
-    """
+    html = bottle.template("templates/index.tpl")
+    return html
 
 bottle.run(host='0.0.0.0', port=sys.argv[1])
 
