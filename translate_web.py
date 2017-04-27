099db6224e11ffb5e7ec8ab836a6be0e76b6e374
kestel@gmail.com <kestel@gmail.com>
2017-04-27 15:38:34 +0300

    Reformate file

--- translate_web.py
+++ translate_web.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.6
+#-*- coding:utf-8 -*-
 
 import sys
 from os import environ
@@ -10,35 +11,37 @@ import bottle
 
 bottle.debug(True)
 
-input_text = None
 
 @bottle.route('/dict')
 def print_dict():
     html = bottle.template("templates/dict.tpl",
-        common_dict=d.common_dict,
-        damage_dict=d.damage_dict,
-        alignment_dict=d.alignment_dict,
-        creature_dict=d.creature_dict,
-        lang_dict=d.lang_dict,
-        condition_dict=d.condition_dict,
-        skill_dict=d.skill_dict,
-        armor_dict=d.armor_dict,
-        weapon_dict=d.weapon_dict )
+                           common_dict=d.common_dict,
+                           damage_dict=d.damage_dict,
+                           alignment_dict=d.alignment_dict,
+                           creature_dict=d.creature_dict,
+                           lang_dict=d.lang_dict,
+                           condition_dict=d.condition_dict,
+                           skill_dict=d.skill_dict,
+                           armor_dict=d.armor_dict,
+                           weapon_dict=d.weapon_dict,
+                           race_dict=d.race_dict,
+                           abilities_dict=d.abilities_dict)
     return html
-    
+
 
 @bottle.route('/spell_dict')
 def print_spell_dict():
     html = bottle.template("templates/spell_dict.tpl",
                            phb_spell=s.phb_spell)
     return html
-    
-    
+
+
 @bottle.post('/translate')
 def print_translate():
-    input_text = bottle.request.forms.get('text')
+    input_text = None
+    input_text = bottle.request.forms.text
     if input_text:
-        return "Вернуться на <a href='/'>главную</a>.<br />\n<pre>"+replacer(input_text, d.all_dict)+"</pre>"
+        return "Вернуться на <a href='/'>главную</a>.<br />\n<pre>" + replacer(input_text, d.all_dict) + "</pre>"
     else:
         return "Вернуться на <a href='/'>главную</a>.<br />\nКажется, вы не ввели текст."
 
@@ -53,5 +56,5 @@ def print_index():
     html = bottle.template("templates/index.tpl", latest_commit_date=latest_commit_date)
     return html
 
-bottle.run(host='0.0.0.0', port=sys.argv[1])
 
+bottle.run(host='0.0.0.0', port=sys.argv[1])
