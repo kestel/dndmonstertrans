50a9ec73e54bc6e8cdf4c5e5d93f5166d071fe53
kestel@gmail.com <kestel@gmail.com>
2017-04-27 15:39:17 +0300

    Add translate for spells.

--- translate.py
+++ translate.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3.6
+#-*- coding:utf-8 -*-
 
 import re
 # подцепляем словарь из внешнего файла
@@ -20,16 +21,25 @@ def replace_lang(text, dic):
 def replace_other(line, dic):
     """Заменяем всё подряд, что не попало под другие фильтры"""
     for i, j in dic.items():
-        line = line.replace(i, j)    
-          
+        line = line.replace(i, j)
+
+    return line
+
+
+def replace_other_lower(line, dic):
+    """Заменяем всё подряд, что не попало под другие фильтры"""
+    for i, j in dic.items():
+        line = line.replace(i.lower(), j.lower())
+
     return line
 
 
 def fix_symbols(line):
     """Эта хрень нужна для исправления отображения разных символов"""
-    line = line.replace(b'\xc3\xa2\xc2\x88\xc2\x92'.decode('utf-8'), "-")
-    line = line.replace(b'\xc3\xa2\xc2\x80\xc2\x99'.decode('utf-8'), "'")
-    line = line.replace("’", "'")
+    line = line.replace(b'\xc3\xa2\xc2\x88\xc2\x92'.decode('utf-8'), "-").\
+        replace(b'\xc3\xa2\xc2\x80\xc2\x99'.decode('utf-8'), "'").\
+        replace('Â¬', '').\
+        replace("’", "'")
     
     return line
 
@@ -50,6 +60,18 @@ def replace_multiattack(line):
 
     return line
 
+
+def translate_spell(line):
+    line = line.replace(";", ",").replace(":", ",")
+    spell_list = line.split(",")
+    newline = ""
+    for spell in spell_list:
+        spell_ru = replace_other_lower(spell.strip(), d.spell_dict)
+        newline = "{0} {1} [{2}], ".format(newline.strip(), spell_ru.strip(), spell.strip())
+
+    return newline[:-2]
+
+
 def replacer(text, dic):
     """Основная функция, которая делает всю работу. В том числе запускает другие функции"""
     translated_text = ""
@@ -66,12 +88,24 @@ def replacer(text, dic):
             
         elif "challenge" in line_lower:
             line = replace_cr(line)
-            
-        """Замена кубов по всему тексту"""
-        line = re.sub(r"(\d+)d(\d+)", r"\1к\2", line)
+
+        elif ("at will" or "cantrip" or "1st level") in line_lower:
+            line = translate_spell(line)
+
+        elif ("2nd level" or "3rd level" or "4th level" or "5th level") in line_lower:
+            line = translate_spell(line)
+
+        elif ("6th level" or "7th level" or "8th level" or "9th level") in line_lower:
+            line = translate_spell(line)
+
+        else:
+            print(line_lower)
 
         """Замена всего оставшегося"""
         line = replace_other(line, dic)
+
+        """Замена кубов по всему тексту"""
+        line = re.sub(r"(\d+)d(\d+)", r"\1к\2", line)
         
         """Собираем текст заново построчно (с виндовым переносом строки)"""
         translated_text = translated_text + "\r\n" + line
